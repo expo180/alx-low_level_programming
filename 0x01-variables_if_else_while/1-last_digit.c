d2a7de1a29c692f6a4117f64b9ec2913802949cd
expo180 <badersaadousalissou@gmail.com>
2023-03-18 12:54:06 +0100

    ouf I get It git add .

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,6 +5,7 @@
 /* betty style doc for function main goes there */
 /**
  * main - Entry point of the function
+ *
  * Return: Always success (0)
 */
 int main(void)
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry level
+ * Return: Always success (0)
+*/
+int main(void)
+{
+	putchar("abcdefghijklmnopqrstuvwxyz");
+	putchar("\n");
+	return (0);
+}
