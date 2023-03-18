f7f052f77dbd0680c077c611c3760618785475e6
expo180 <badersaadousalissou@gmail.com>
2023-03-18 17:22:01 +0100

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry of the function
+ *
+ * Return: Always success (0)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
