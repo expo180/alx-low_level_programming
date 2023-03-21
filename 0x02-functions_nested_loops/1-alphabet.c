36cdf479721908090b577028d289c7ec49990c1f
expo180 <badersaadousalissou@gmail.com>
2023-03-21 19:07:02 +0100

    Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry of the program
+ *
+ * Return: Success(0)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
