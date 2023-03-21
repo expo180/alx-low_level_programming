e9161ab1f9c46eb339bd603e47d7d4156daa1b84
expo180 <badersaadousalissou@gmail.com>
2023-03-21 12:55:13 +0100

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry of the program
+ *
+ * Return: Success(0)
+ */
+int main(void)
+{
+	char c = '_', d = 'p', e = 'u', f = 't', g = 'c', h = 'h', i = 'a', j = 'r';
+
+	putchar(c);
+	putchar(d);
+	putchar(e);
+	putchar(f);
+	putchar(g);
+	putchar(h);
+	putchar(i);
+	putchar(j);
+	putchar('\n');
+	return (0);
+
+}
