76a3b5222bb20f99f6a9b1854361d1f638a476b0
expo180 <badersaadousalissou@gmail.com>
2023-03-22 01:25:24 +0100

    Late night code

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+
+/**
+ * add - Adds two values
+ * @n1:  first number
+ * @n2:  second number 
+ * Return: Integer value
+ */
+
+int add(int c1, int c2)
+
+{
+
+	return (c1 + c2);
+
+}
+
