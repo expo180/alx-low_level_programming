59cc8ebba8f186462b686e04122b5718e8ebe9fb
expo180 <badersaadousalissou@gmail.com>
2023-03-22 10:04:51 +0100

    main

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - create an infinite loop
+* Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	j = 0;
+	/**
+	 * while (j < 10)
+	 * {
+	 * putchar(j);
+	 * }
+	 */
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
