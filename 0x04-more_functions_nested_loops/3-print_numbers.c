3aac5ff30cb9ee4654764866b71e11f12b5e8f6d
expo180 <badersaadousalissou@gmail.com>
2023-03-23 22:21:05 +0100

    The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include "_putchar.c"
+/**
+ * print_numbers - Prints all the numbers
+ *
+ * Return: Success(0)
+ */
+
+void print_numbers(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+		_putchar(i);
+	_putchar('\n');
+}
