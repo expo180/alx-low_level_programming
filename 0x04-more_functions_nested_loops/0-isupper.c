50c2bad484ce55d85498476e9d23a774a8142ff1
expo180 <badersaadousalissou@gmail.com>
2023-03-23 17:51:45 +0100

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * is_upper - This function checks if a character is uppercase or not
+ *
+ * Return: 1 if c is uppercase
+ */
+int _isupper(int c)
+{
+	int c;
+
+	if (c >= 'A' && c <= 'Z')
+		return 1;
+	else
+		return (0);
+	_purchar('\n');
+}
