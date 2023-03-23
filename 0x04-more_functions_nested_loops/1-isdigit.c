90b26d7baa2006099bcf43bc11e042c67506be81
expo180 <badersaadousalissou@gmail.com>
2023-03-23 18:57:37 +0100

    1. isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -15,18 +15,3 @@ int _isdigit(int c)
 		return (0);
 	putchar('\n');
 }
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    char c;
-
-    c = '0';
-    printf("%c: %d\n", c, _isdigit(c));
-    c = 'a';
-    printf("%c: %d\n", c, _isdigit(c));
-    return (0);
-}
